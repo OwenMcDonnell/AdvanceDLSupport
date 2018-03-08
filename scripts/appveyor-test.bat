2979804e34e7936e13816f7caebaaf519faf539c
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-03-08 13:54:41 -0800

    Update appveyor-test.bat

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -1,4 +1,9 @@
 @echo off
 
-dotnet test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
-dotnet test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+if "%PLATFORM%"=="x86" (
+	"C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
+  "C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+) else (
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+)
