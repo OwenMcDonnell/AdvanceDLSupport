4fde3cd421c8e8733f4761e714ebfd8e5787fc00
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-03-08 17:31:20 -0800

    Update appveyor-test.bat

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -2,7 +2,12 @@
 if "%PLATFORM%"=="x86" (
   "C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
   "C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
-) else (
+) 
+if "%PLATFORM%"=="x64" (
   "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
   "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+)
+if "%PLATFORM%"=="Any cpu" (
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests -o AdvancedDLSupport.tests/bin/Debug/netcoreapp2.0/
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests -o Mono.DllMap.Tests/bin/Debug/netcoreapp2.0/
 )
