51c3a84f1f7911b440b6c77d0960eae2bf22cb2f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-03-08 16:45:16 -0800

    Update appveyor-test.bat

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -1,8 +1,8 @@
 @echo off
-set TEMP_PLATFORM="%PLATFORM%"
-set PLATFORM=
-
-
-dotnet test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
-dotnet test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
-set PLATFORM="%TEMP_PLATFORM%"
+if "%PLATFORM%"=="x86" (
+  "C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
+  "C:\Program Files (x86)\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+) else (
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
+  "C:\Program Files\dotnet\dotnet.EXE" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+)
